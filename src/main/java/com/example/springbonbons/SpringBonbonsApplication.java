a0bc889c1a6e787cd21536b429e30ffca6e2a1ea
GreenHawk <greenhawk99.mb@gmail.com>
2022-04-19 15:24:33 +0200

    Projet vue en formation

---
+++ src/main/java/com/example/springbonbons/SpringBonbonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbonbons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBonbonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBonbonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbonbons/SpringBonbonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbonbons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBonbonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
